76a339c07e846162f5830d59076575912444f659
snozzy8z <atoxmillenium@gmail.com>
2020-03-22 21:01:52 +0100

    last commit

--- Evaluation/exercice 2/test.js
+++
@@ -1,8 +0,0 @@
-var n = parseInt(prompt("Saisissez un nombre entier N"));
-var i = 0;
-var r = 0;
-//Je fais une boucle for qui viens incrémenter le compteur i.
-for (i; i <= n; i++)
-//J'addition le résultat (r = r + i;)
-    r += i;
-alert("la somme des entiers N est de " + r);
